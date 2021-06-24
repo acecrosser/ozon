6f70e02920ed277f69d966174b05494668f5928b
acecrosser <acecrosser@yandex.ru>
2021-06-24 13:14:38 +0300

    sql

--- init.sql
+++ init.sql
@@ -6,8 +6,8 @@ CREATE DATABASE shorturl
     WITH 
     OWNER = postgres
     ENCODING = 'UTF8'
-    LC_COLLATE = 'Russian_Russia.1251'
-    LC_CTYPE = 'Russian_Russia.1251'
+    LC_COLLATE = 'ru_RU.UTF-8'
+    LC_CTYPE = 'ru_RU.UTF-8'
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
